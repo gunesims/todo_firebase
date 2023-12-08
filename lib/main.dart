f2c9b86b6b0b8077f85f74470ddefe237eecf9e5
timurIsakov <timur092000@gmail.com>
2023-12-08 12:01:41 +0500

    Added locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_firebase/locator.dart';
 
 import 'application/application.dart';
 import 'firebase_options.dart';
@@ -9,5 +10,6 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  setup();
   runApp(const Application());
 }
